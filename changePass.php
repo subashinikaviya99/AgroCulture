c698bc5928cb7501a20798cda4b7b92a9b5d09ca
subashinikaviya99 <79144024+subashinikaviya99@users.noreply.github.com>
2021-02-16 17:24:38 +0530

    Initial commit

---
+++ changePass.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+
+    require 'db.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $user = dataFilter($_POST['fusername']);
+        $currPass = $_POST['currPass'];
+        $newPass = $_POST['newPass'];
+        $conNewPass = $_POST['conNewPass'];
+        $newHash = dataFilter( md5( rand(0,1000) ) );
+
+    }
+
+    $sql = "SELECT * FROM farmer WHERE fusername='$user'";
+    $result = mysqli_query($conn, $sql);
+    $num_rows = mysqli_num_rows($result);
+
+    if($num_rows == 0)
+    {
+        $_SESSION['message'] = "Invalid User Credentials!";
+        header("location: Login/error.php");
+    }
+    else
+    {
+        $User = $result->fetch_assoc();
+
+        if(password_verify($_POST['currPass'], $user['Password']))
+        {
+            if($newPass == $conNewPass)
+            {
+                $conNewPass = dataFilter(password_hash($_POST['conNewPass'], PASSWORD_BCRYPT));
+                $currHash = $_SESSION['Hash'];
+                $sql = "UPDATE members SET Password='$conNewPass', Hash='$newHash' WHERE Hash='$currHash';";
+
+                $result = mysqli_query($conn, $sql);
+
+                if($result)
+                {
+                    $_SESSION['message'] = "Password changed Successfully!";
+                    header("location: ../Login/success.php");
+                }
+                else
+                {
+                    $_SESSION['message'] = "Error occurred while changing password<br>Please try again!";
+                    header("location: ../Login/error.php");
+                }
+            }
+        }
+        else
+        {
+            $_SESSION['message'] = "Invalid current User Credentials!";
+            header("location: ../Login/error.php");
+        }
+    }
+
+    function dataFilter($data)
+    {
+    	$data = trim($data);
+     	$data = stripslashes($data);
+    	$data = htmlspecialchars($data);
+      	return $data;
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,340 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>AgroCulture</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap\js\bootstrap.min.js"></script>
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="login.css"/>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<link rel="stylesheet" href="indexfooter.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+
+	<?php
+		require 'menu.php';
+	?>
+
+		<!-- Banner -->
+			<section id="banner" class="wrapper">
+				<div class="container">
+				<h2>AgroCulture</h2>
+				<p>Your Product Our Market</p>
+				<br><br>
+				<center>
+					<div class="row uniform">
+						<div class="6u 12u$(xsmall)">
+							<button class="button fit" onclick="document.getElementById('id01').style.display='block'" style="width:auto">LOGIN</button>
+						</div>
+
+						<div class="6u 12u$(xsmall)">
+							<button class="button fit" onclick="document.getElementById('id02').style.display='block'" style="width:auto">REGISTER</button>
+						</div>
+					</div>
+				</center>
+
+
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1 align-center">
+				<div class="container">
+					<header>
+						<h2>AgroCulture</h2>
+						<p>Explore the new way of trading...</p>
+					</header>
+					<div class="row 200%">
+						<section class="4u 12u$(small)">
+							<i class="icon big rounded fa-clock-o"></i>
+							<p>Digital Market</p>
+						</section>
+						<section class="4u 12u$(small)">
+							<i class="icon big rounded fa-comments"></i>
+							<p>Agro-Blog</p>
+						</section>
+						<section class="4u$ 12u$(small)">
+							<i class="icon big rounded fa-user"></i>
+							<p>Register with us</p>
+						</section>
+					</div>
+				</div>
+			</section>
+
+
+		<!-- Footer -->
+		<footer class="footer-distributed" style="background-color:black" id="aboutUs">
+		<center>
+			<h1 style="font: 35px calibri;">About Us</h1>
+		</center>
+		<div class="footer-left">
+			<h3 style="font-family: 'Times New Roman', cursive;">AgroCulture &copy; </h3>
+		<!--	<div class="logo">
+				<a href="index.php"><img src="images/logo.png" width="200px"></a>
+			</div>-->
+			<br />
+			<p style="font-size:20px;color:white">Your product Our market !!!</p>
+			<br />
+		</div>
+
+		<div class="footer-center">
+			<div>
+				<i class="fa fa-map-marker"></i>
+				<p style="font-size:20px">Agro Culture Fam<span>Vormir</span></p>
+			</div>
+			<div>
+				<i class="fa fa-phone"></i>
+				<p style="font-size:20px">123456789</p>
+			</div>
+			<div>
+				<i class="fa fa-envelope"></i>
+				<p style="font-size:20px"><a href="mailto:agroculture@gmail.com" style="color:white">demo@demo.com</a></p>
+			</div>
+		</div>
+
+		<div class="footer-right">
+			<p class="footer-company-about" style="color:white">
+				<span style="font-size:20px"><b>About AgroCulture</b></span>
+				AgroCulture is e-commerce trading platform for grains & grocerries...
+			</p>
+			<div class="footer-icons">
+				<a  href="#"><i style="margin-left: 0;margin-top:5px;"class="fa fa-facebook"></i></a>
+				<a href="#"><i style="margin-left: 0;margin-top:5px" class="fa fa-instagram"></i></a>
+				<a href="#"><i style="margin-left: 0;margin-top:5px" class="fa fa-youtube"></i></a>
+			</div>
+		</div>
+
+	</footer>
+
+
+			<div id="id01" class="modal">
+
+  <form class="modal-content animate" action="Login/login.php" method='POST'>
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container">
+    <h3>Login</h3>
+							<form method="post" action="Login/login.php">
+								<div class="row uniform 50%">
+									<div class="7u$">
+										<input type="text" name="uname" id="uname" value="" placeholder="UserName" style="width:80%" required/>
+									</div>
+									<div class="7u$">
+										<input type="password" name="pass" id="pass" value="" placeholder="Password" style="width:80%" required/>
+									</div>
+								</div>
+									<div class="row uniform">
+										<p>
+				                            <b>Category : </b>
+				                        </p>
+				                        <div class="3u 12u$(small)">
+				                            <input type="radio" id="farmer" name="category" value="1" checked>
+				                            <label for="farmer">Farmer</label>
+				                        </div>
+				                        <div class="3u 12u$(small)">
+				                            <input type="radio" id="buyer" name="category" value="0">
+				                            <label for="buyer">Buyer</label>
+				                        </div>
+									</div>
+									<center>
+									<div class="row uniform">
+										<div class="7u 12u$(small)">
+											<input type="submit" value="Login" />
+										</div>
+									</div>
+									</center>
+								</div>
+							</form>
+						</section>
+</div>
+    </div>
+    </div>
+  </form>
+</div>
+
+
+<div id="id02" class="modal">
+
+  <form class="modal-content animate" action="Login/signUp.php" method='POST'>
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container">
+
+<section>
+							<h3>SignUp</h3>
+							<form method="post" action="Login/signUp.php">
+							<script>
+								function mobilenumbervalidity()
+								{
+									var mn = document.getElementById("phone").value;
+									if (mn.length==10)
+									{
+										for (var i = 0; i < mn.length; i++) 
+										{
+											if(!(mn.charAt(i)>=0 && mn.charAt(i)<=9))
+											{
+												alert("Mobile Number should contain only Digits");
+												document.getElementById("phone").value = ""
+												document.getElementById("phone").focus();
+											}
+										}
+									}
+									else
+									{
+										alert("Mobile Number should be 10 Digit");
+										document.getElementById("phone").value = ""
+										document.getElementById("phone").focus();
+									}
+								}
+								function passwordvalidity()
+								{
+									var pwd = document.getElementById("password").value;
+									if (pwd.length<8)
+									{
+										alert("Password length should contain 8 characters");
+										document.getElementById("password").value = ""
+										document.getElementById("password").focus();
+									}
+									else
+									{
+									var p1 = /[A-Z]/g;
+									if(!pwd.match(p1))
+									{
+										alert("Password should contain atleast one upper case character");
+										document.getElementById("password").value = ""
+										document.getElementById("password").focus();										
+									}
+									var p2 = /[a-z]/g;
+									if(!pwd.match(p2))
+									{
+										alert("Password should contain atleast one lower case character");	
+										document.getElementById("password").value = ""
+										document.getElementById("password").focus();
+									}
+									var p3 = /[0-9]/g;
+									if(!pwd.match(p3))
+									{
+										alert("Password should contain atleast one digit");																			
+										document.getElementById("password").value = ""
+										document.getElementById("password").focus();
+									}
+									}
+								}
+								function repasswordvalidity()
+								{
+									// retype_password
+									if(document.getElementById("password").value != document.getElementById("retype_password").value)
+									{
+										alert("Password and Retype password should be same");	
+										document.getElementById("retype_password").value = ""
+										document.getElementById("retype_password").focus();
+									}
+								}
+							</script>
+								<center>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="name" id="name" value="" placeholder="Name" required/>
+									</div>
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="uname" id="uname" value="" placeholder="UserName" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="phone" id="phone" value="" placeholder="Mobile Number" required  onfocusout = "mobilenumbervalidity()"/>
+										<small>Format:1234567890</small>
+									</div>
+
+									<div class="3u 12u$(xsmall)">
+										<input type="email" name="email" id="email" value="" name="emails" placeholder="Email" multiple required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+			                            <input type="password" name="password" id="password" value="" placeholder="Password" required onfocusout="passwordvalidity()"/>
+			                        </div>
+			                        <div class="3u 12u$(xsmall)">
+			                            <input type="password" name="pass" id="retype_password" value="" placeholder="Retype Password" required onfocusout="repasswordvalidity()"/>
+			                        </div>
+								</div>
+								<div class="row uniform">
+									<div class="6u 12u$(xsmall)">
+										<input type="text" name="addr" id="addr" value="" placeholder="Address" style="width:80%" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<p>
+			                            <b>Category : </b>
+			                        </p>
+			                        <div class="3u 12u$(small)">
+			                            <input type="radio" id="farmer" name="category" value="1" checked>
+			                            <label for="farmer">Farmer</label>
+			                        </div>
+			                        <div class="3u 12u$(small)">
+			                            <input type="radio" id="buyer" name="category" value="0">
+			                            <label for="buyer">Buyer</label>
+			                        </div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(small)">
+										<input type="submit" value="Submit" name="submit" class="special" /></li>
+									</div>
+									<div class="3u 12u$(small)">
+										<input type="reset" value="Reset" name="reset"/></li>
+									</div>
+								</div>
+							</center>
+							</form>
+						</section>
+
+    </div>
+    </div>
+  </form>
+</div>
+
+
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal1= document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+</script>
+
+
+	</body>
+</html>
